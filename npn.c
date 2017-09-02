4f78de2061a07f4e7002c4bc032b835ba97de765
pnathiya78 <31504004+pnathiya78@users.noreply.github.com>
2017-09-02 15:40:51 +0530

    Create npn.c

---
+++ npn.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("Enter the number");
+scanf("%d",&n);
+if(n>=0)
+{
+ if(n==0)
+ {
+  printf("the number is zero");
+   }
+ else
+ {
+   printf("the number is positive");
+  }
+ }  
+else
+{
+printf("the number is negative"); 
+}
+}
