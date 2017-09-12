9111dddb0f3d232ef6e57daa8cc0ca47043f35ac
pnathiya78 <31504004+pnathiya78@users.noreply.github.com>
2017-09-12 19:57:23 +0530

    Update nan.c

--- nan.c
+++ nan.c
@@ -5,7 +5,7 @@ int main()
 char ch;
 printf("enter the alphabet");
 scanf("%c",&ch);
-if((ch>=65&&ch<=90)||(ch>=97||ch<=122))
+if(((ch>=65)&&(ch<=90))||((ch>=97)||(ch<=122)))
 {
 printf("enter character is alphabet");
 }
