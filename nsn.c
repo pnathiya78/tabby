597bb17e3ef4b83451229da84a2f68672edc9428
pnathiya78 <31504004+pnathiya78@users.noreply.github.com>
2017-09-14 20:07:02 +0530

    Create nsn.c

---
+++ nsn.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,n,sum=0;
+printf("enter the vale of n");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+sum=sum+i;
+}
+printf("sum of n number is %d",sum);
+retrn 0;
+}
