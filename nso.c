a59ea736a869bdbc55d93cb9bff86c0b75664b25
pnathiya78 <31504004+pnathiya78@users.noreply.github.com>
2017-11-29 19:00:31 +0530

    Create nso.c

---
+++ nso.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,sum=0,os=0;
+for(i=1;i<=15;i++)
+{
+sum=sum+i;
+}
+printf("the sum is %d",sum);
+for(i=15;i<=45;i++)
+{
+if((i%2)!=0)
+{
+os=os+i;
+}
+else
+{
+}
+}
+printf("the sum of odd no is %d",os);
+getch():
+}
