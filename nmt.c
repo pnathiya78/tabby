c2617feb40fae029bc3edc54b911a418a8054a72
pnathiya78 <31504004+pnathiya78@users.noreply.github.com>
2017-11-29 19:25:31 +0530

    Create nmt.c

---
+++ nmt.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,a;
+printf("enter the number");
+scanf("%d",&n);
+for(i=1;i<=5;i++)
+{
+n*i=a;
+printf("%d*%d=%d",n,i,a);
+}
+getch();
+}
