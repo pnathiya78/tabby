f3c226e047b7a120b2e55789ac2c23bfe5d0c60a
pnathiya78 <31504004+pnathiya78@users.noreply.github.com>
2017-12-07 19:37:35 +0530

    Create cnc.c

---
+++ cnc.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch[200],c=1,i=0;
+printf("enter the string");
+gets(ch);
+if(ch[i]!=0)
+{
+swithch(ch[i])
+{
+case 'a':c++;break;
+case 'b':c++;break;
+case 'c':c++;break;
+case 'd':c++;break;
+case 'e':c++;break;
+case 'f':c++;break;
+case 'g':c++;break;
+case 'h':c++;break;
+case 'i':c++;break;
+case 'j':c++;break;
+case 'k':c++;break;
+case 'l':c++;break;
+case 'm':c++;break;
+case 'n':c++;break;
+case 'o':c++;break;
+case 'p':c++;break;
+case 'q':c++;break;
+case 'r':c++;break;
+case 's':c++;break;
+case 't':c++;break;
+case 'u':c++;break;
+case 'v':c++;break;
+case 'w':c++;break;
+case 'x':c++;break;
+case 'y':c++;break;
+case 'z':c++;break;
+case 'A':c++;break;
+case 'B':c++;brea;
+case 'C':c++;break;
+case 'D':c++;break;
+case 'E':c++;brea;
+case 'F':c++;break;
+case 'G':c++;break;
+case 'H':c++;break;
+case 'I':c++;break;
+case 'J':c++;break;
+case 'K':c++;break;
+case 'L':c++;brea;
+case 'M':c++;break;
+case 'N':c++;break;
+case 'O':c++;break;
+case 'P':c++;break;
+case 'Q':c++;break;
+case 'R':c++;break;
+case 'S':c++;break;
+case 'T':c++;break;
+case 'U':c++;break;
+case 'V':c++;break;
+case 'W':c++;break;
+case 'X':c++;break;
+case 'Y':c++;break;
+case 'Z':c++;break;
+}
+i++;
+}
+else
+{
+i++;
+}
+printf("%d",c);
+}
