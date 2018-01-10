f7e44a5c42a920fd849212b6f75343aa037227db
pnathiya78 <31504004+pnathiya78@users.noreply.github.com>
2018-01-10 20:03:24 +0530

    Update nlp.c

--- nlp.c
+++ nlp.c
@@ -3,10 +3,18 @@
 #include<string.h>
 void main()
 {
-char ch[500],i,c=0;
+char ch[500],i=0,c=0;
 gets(ch);
+while(ch[i]!=0)
+{
 if(ch[i]=='.')
 {
-c++;
+  c++;
 }
+  else
+  {
+}
+  i++;
+}
+printf("%d",c);
 }
