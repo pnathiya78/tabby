eaa60c52bcd6c0a0fcb91c1c9ea7ee3aa6ed3d29
pnathiya78 <31504004+pnathiya78@users.noreply.github.com>
2018-01-21 11:02:28 +0530

    Create n44.c

---
+++ n44.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n;
+printf("enter the number:");
+scanf("%d",&n);
+if((n>=1)&&(n<=10))
+{
+printf("\nyes,it is in the range");
+}
+else
+{
+printf("\nno,it is not in the range");
+}
+}
