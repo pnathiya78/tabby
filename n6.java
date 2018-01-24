36ca8897a22f494e78db2c035d4781db8017651d
pnathiya78 <31504004+pnathiya78@users.noreply.github.com>
2018-01-24 20:53:26 +0530

    Create n6.java

---
+++ n6.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class n6{
+public static void main(String a[])
+{
+int b;
+Scanner n=new Scanner(System.in);
+b=n.nextInt();
+if((b%4)==0)
+{
+System.out.println("the year %d is a leap year",b);
+}
+else
+{
+System.out.println("the year %d is not a leap year",b);
+}
+}
