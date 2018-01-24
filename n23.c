adb4d12297eb90ba8cb290ce5c2257fa60ea6133
pnathiya78 <31504004+pnathiya78@users.noreply.github.com>
2018-01-24 16:20:23 +0530

    Create n23.c

---
+++ n23.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,b[n],i;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&b[i]);
+}
+for(i=0;i<n;i++)
+{
+if(b[i]<b[i+1])
+{
+}
+else
+{
+b[i]=b[i]+b[i+1];
+b[i+1]=b[i]-b[i+1];
+b[i]=b[i]-b[i+1];
+}
+}
+printf("%d",b[0]);
+}
