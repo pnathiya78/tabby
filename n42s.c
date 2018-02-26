8bc210bc7b82129a61d2c706f1f5dae91c3acc23
pnathiya78 <31504004+pnathiya78@users.noreply.github.com>
2018-02-26 16:40:01 +0530

    Rename n42.c to n42s.c

---
+++ n42s.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void) {
+	char c[50];
+	char d[50];
+	scanf("%s",&c);
+	scanf("%s",&d);
+	int i=0,j=0;
+while(c[i]!='\0')
+	{
+
+		++i;
+	}
+while(d[j]!='\0')
+	{
+	
+		++j;
+	}
+	 if(i>j)
+	{
+		printf("\n%s",c);
+	}
+else if(j>i)
+	{
+		printf("\n%s",d);
+	}
+	else
+	{
+		printf("equal");
+	}
+	return 0;
+}
+
