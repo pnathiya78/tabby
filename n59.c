4088e263e2e4322e77fe083eb58bcdf42f6957fb
pnathiya78 <31504004+pnathiya78@users.noreply.github.com>
2018-03-12 16:22:06 +0530

    Create n59.c

---
+++ n59.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+	int a[10],i,t,j;
+	for(i=0;i<10;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<10;i++)
+	{
+		for(j=i+1;j<10;j++)
+		{
+			if(a[i]>a[j])
+			{
+				t=a[i];
+				a[i]=a[j];
+				a[j]=t;
+			}
+		}
+	}
+	printf("%d",a[10-1]);
+	return 0;
+}
