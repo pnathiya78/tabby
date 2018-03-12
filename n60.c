e17576119bcf190df1177b572cac0704ba6b43d4
pnathiya78 <31504004+pnathiya78@users.noreply.github.com>
2018-03-12 16:29:16 +0530

    Create n60.c

---
+++ n60.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int t1=0,t2=1,range,nt;
+	printf("enter the range:");
+	scanf("%d",&range);
+	printf("%d %d",t1,t2);
+	while(nt<range)
+	{
+		nt=t1+t2;
+		printf(" %d",nt);
+		t1=t2;
+		t2=nt;
+	}
+	return 0;
+}
