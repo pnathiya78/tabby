444d930122382cddfac4de8c2cdf9c6a39e0ed14
pnathiya78 <31504004+pnathiya78@users.noreply.github.com>
2018-03-14 15:55:05 +0530

    Create n64.c

---
+++ n64.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+         int n,m,c;
+         scanf("%d%d",&n,&m);
+         c=n+m;
+         if((c%2)==0)
+         {
+         	printf("the result is even");
+         }
+         else
+         {
+         	printf("the result is odd");
+         }
+	return 0;
+}
